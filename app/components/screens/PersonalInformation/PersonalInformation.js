0d1e57c64c65aa0eaee4879023836e7b54882d56
nemanja <nemanjakrstonic2@gmail.com>
2019-08-20 12:52:38 +0200

    CN-3619 Add Building Number
    EarnInterestInCel Modal padding fix

--- app/components/screens/PersonalInformation/PersonalInformation.js
+++ app/components/screens/PersonalInformation/PersonalInformation.js
@@ -64,112 +64,119 @@ class PersonalInformation extends Component {
       <RegularLayout>
         <CelText margin={"0 0 20 0"} align={"center"} weight={"300"} type={"H4"}>To make changes on your personal
           information <CelText weight={"300"} type={"H4"} color={STYLES.COLORS.CELSIUS_BLUE}
-                               onPress={() => Linking.openURL("mailto:app@celsius.network")}>contact our
+            onPress={() => Linking.openURL("mailto:app@celsius.network")}>contact our
             support.</CelText></CelText>
         {isUSCitizen() && (
           <View>
             <Separator margin={"10 0 20 0"} color={STYLES.COLORS.DARK_GRAY} opacity={0.2} textOpacity={0.4}
-                       text={"SOCIAL SECURITY NUMBER"}/>
+              text={"SOCIAL SECURITY NUMBER"} />
 
             {!user.ssn &&
-            <View>
-              <CelText margin={"0 0 20 0"} type={"H4"} align={"left"} weight={"300"}>
-                We are required to collect SSN from all American users. Please provide your SSN to start earning
-                interest.
-                This information is encrypted and highly secured.
+              <View>
+                <CelText margin={"0 0 20 0"} type={"H4"} align={"left"} weight={"300"}>
+                  We are required to collect SSN from all American users. Please provide your SSN to start earning
+                  interest.
+                  This information is encrypted and highly secured.
               </CelText>
-            </View>
+              </View>
             }
-            <SocialSecurityNumber 
-            onPress={() => this.updateSSNNumber()}
+            <SocialSecurityNumber
+              onPress={() => this.updateSSNNumber()}
             />
           </View>
         )}
 
         <Separator margin={"10 0 20 0"} color={STYLES.COLORS.DARK_GRAY} opacity={0.2} textOpacity={0.4}
-                   text={"PROFILE DETAILS"}/>
+          text={"PROFILE DETAILS"} />
 
         <View>
           <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>First name</CelText>
-          <CelInput field={"profileFirst"} disabled value={user.first_name}/>
+          <CelInput field={"profileFirst"} disabled value={user.first_name} />
         </View>
 
         <View>
           <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Last name</CelText>
-          <CelInput field={"profileLast"} disabled value={user.last_name}/>
+          <CelInput field={"profileLast"} disabled value={user.last_name} />
         </View>
 
         <View>
           <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Email</CelText>
-          <CelInput field={"profileEmail"} disabled type="text" value={user.email}/>
+          <CelInput field={"profileEmail"} disabled type="text" value={user.email} />
         </View>
 
         {user.date_of_birth &&
-        <View>
-          <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Date of birth</CelText>
-          <View style={style.addressInfo}>
-            <CelInput field={"profileMonth"} margin={"0 20 0 0"} large={false} disabled type="text"
-                      value={moment([dateOfBirth[0], dateOfBirth[1]].join("-")).format("MMM")}/>
-            <CelInput field={"profileDay"} margin={"0 20 0 0"} large={false} disabled type="text"
-                      value={dateOfBirth[2]}/>
-            <CelInput field={"profileYear"} margin={"0 20 0 0"} large={false} disabled type="text"
-                      value={dateOfBirth[0]}/>
+          <View>
+            <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Date of birth</CelText>
+            <View style={style.addressInfo}>
+              <CelInput field={"profileMonth"} margin={"0 20 0 0"} large={false} disabled type="text"
+                value={moment([dateOfBirth[0], dateOfBirth[1]].join("-")).format("MMM")} />
+              <CelInput field={"profileDay"} margin={"0 20 0 0"} large={false} disabled type="text"
+                value={dateOfBirth[2]} />
+              <CelInput field={"profileYear"} margin={"0 20 0 0"} large={false} disabled type="text"
+                value={dateOfBirth[0]} />
+            </View>
           </View>
-        </View>
         }
 
         {user.gender &&
-        <View>
-          <CelText margin={"10 0 10 0"} type={"H4"} weight={"300"}>Gender</CelText>
-          <CelInput field={"profileGender"} disabled type="text" value={user.gender}/>
-        </View>
+          <View>
+            <CelText margin={"10 0 10 0"} type={"H4"} weight={"300"}>Gender</CelText>
+            <CelInput field={"profileGender"} disabled type="text" value={user.gender} />
+          </View>
         }
 
         {user.citizenship && <View>
           <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Citizenship</CelText>
-          <CelInput field={"profileCitizenship"} disabled type="text" value={user.citizenship}/>
+          <CelInput field={"profileCitizenship"} disabled type="text" value={user.citizenship} />
         </View>}
 
         {user.cellphone &&
-        <CelInput type="text" field='profileCellphone' disabled placeholder='Phone number' error={formErrors.cellphone}
-                  value={user.cellphone_verified ? user.cellphone : ""} margin={"0 0 20 0"}/>
+          <CelInput type="text" field='profileCellphone' disabled placeholder='Phone number' error={formErrors.cellphone}
+            value={user.cellphone_verified ? user.cellphone : ""} margin={"0 0 20 0"} />
         }
 
         {!user.cellphone_verified &&
-        <CelButton
-          margin={"20 0 20 0"}
-          onPress={() => actions.navigateTo("CellphoneEnter")}
-        >
-          Enter Phone Number
+          <CelButton
+            margin={"20 0 20 0"}
+            onPress={() => actions.navigateTo("CellphoneEnter")}
+          >
+            Enter Phone Number
         </CelButton>
         }
 
         <Separator margin={"10 0 20 0"} color={STYLES.COLORS.DARK_GRAY} opacity={0.2} textOpacity={0.4}
-                   text={"ADDRESS INFO"}/>
+          text={"ADDRESS INFO"} />
 
         <View>
-          <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Address</CelText>
-          <CelInput field={"profileStreet"} disabled type="text" value={user.street}/>
+          <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Street address</CelText>
+          <CelInput field={"profileStreet"} disabled type="text" value={user.street} />
         </View>
+
+        {user.building_number &&
+          <View>
+            <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Building number</CelText>
+            <CelInput field={"buildingNumber"} disabled type="text" value={user.building_number} />
+          </View>
+        }
 
         <View>
           <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Apartment number</CelText>
-          <CelInput field={"profileApartment"} disabled type="text" value={user.flat_number}/>
+          <CelInput field={"profileApartment"} disabled type="text" value={user.flat_number} />
         </View>
 
         <View>
           <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>City</CelText>
-          <CelInput field={"profileCity"} disabled type="text" value={user.city}/>
+          <CelInput field={"profileCity"} disabled type="text" value={user.city} />
         </View>
 
         <View>
           <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>ZIP / Postal Code</CelText>
-          <CelInput field={"profileZip"} disabled type="text" value={user.zip}/>
+          <CelInput field={"profileZip"} disabled type="text" value={user.zip} />
         </View>
 
         <View>
           <CelText margin={"0 0 10 0"} type={"H4"} weight={"300"}>Country</CelText>
-          <CelInput field={"profileCountry"} disabled type="text" value={user.country}/>
+          <CelInput field={"profileCountry"} disabled type="text" value={user.country} />
         </View>
 
       </RegularLayout>
